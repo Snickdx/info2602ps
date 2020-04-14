4c2d6108351b472425c172b35f54ca7c7c0f5326
Snickdx <snickdx@gmail.com>
2020-04-14 16:22:29 -0400

    seconde commit

--- main.py
+++ main.py
@@ -21,6 +21,31 @@ app.app_context().push()
 
 ''' End Boilerplate Code '''
 
+@app.route('/data', methods=['GET'])
+def getData():
+  token = request.args.get('token')
+  res = 'Hello token='+token if token else "Hello"
+  return res
+
+@app.route('/data', methods=['POST'])
+def addData():
+  data = request.json
+  res = 'Hello data='+json.dumps(data) if data else "Hello"
+  return res, 201
+
+@app.route('/data/:id', methods=['DELETE'])
+def removeData(id):
+  res = 'id '+id+' Deleted!'
+  return res, 204
+
+@app.route('/data/:id', methods=['UPDATE'])
+def updateData(id):
+  data = request.json
+  res = 'id '+id
+  res += ' Hello data='+json.dumps(data) if data else "Hello"
+  return res, 201
+
+
 @app.route('/')
 def index():
   return render_template('home.html')
